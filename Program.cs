9c1e0b3bdbf88373aa6086abde285768f85c20d8
phillip deleon <phillipdeleon68@gmail.com>
2024-12-02 14:42:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Assignment_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to get the sum: ");
+            int numInput = Convert.ToInt32(Console.ReadLine());
+
+            int result = SumOfString(numInput);
+
+            Console.WriteLine($"The Sum of {numInput} is {result}");
+            
+        }
+        //create a method that converts int to string and adds the sum
+        static int SumOfString(int num)
+        {
+            int sum = 0;
+            string intString = num.ToString();
+
+            foreach (char number in intString)
+            {
+                if (number != '-')
+                {
+                    sum += number - '0'; 
+                }
+            }
+            return sum;
+        }
+    }
+}
